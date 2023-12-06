e40dc84068970dab10fd44e693302d29d8293654
Kat <klepticat@gmail.com>
2023-12-06 03:15:56 -0500

    Add config

--- IncreaseShipCap.cs
+++ IncreaseShipCap.cs
@@ -1,14 +1,38 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
 
 namespace IncreaseShipCap
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class IncreaseShipCapPlugin : BaseUnityPlugin
     {
+        static Harmony _harmony;
+
+        public static ConfigEntry<int> configMaxItemCount;
+
         private void Awake()
         {
+            configMaxItemCount = Config.Bind<int>("General",
+                                                  "MaxItemCount",
+                                                  200,
+                                                  "Number of max items on the ship.");
+
+            _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            _harmony.PatchAll(typeof(Patches));
+
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-            StartOfRound.Instance.maxShipItemCapacity = 200;
+            Logger.LogInfo($"Ship Item Cap set to {configMaxItemCount.Value}");
+        }
+
+        
+
+    }
+
+    public class Patches {
+        [HarmonyPatch(typeof(StartOfRound), "Awake")]
+        static void Postfix(ref int ___maxShipItemCapacity) {
+            ___maxShipItemCapacity = IncreaseShipCapPlugin.configMaxItemCount.Value;
         }
     }
 }
